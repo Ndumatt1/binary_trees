3b101db1bfe3ed7ae262245e61f38eaaa52f4df4
Ndumatt1 <ndubuisimathew8@gmail.com>
2023-02-28 21:03:11 +0100

    upload 1-binary_tree_insert_left

--- binary_trees.h
+++ binary_trees.h
@@ -2,6 +2,7 @@
 #define BINARY_TRESS_H
 
 #include <stddef.h>
+#include <stdlib.h>
 
 /**
  * struct binary_tree_s - Binary tree node
@@ -25,5 +26,6 @@ void binary_tree_print(const binary_tree_t *);
 
 /* Task prototypes */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 
 #endif /* BINARY_TREES_H */
