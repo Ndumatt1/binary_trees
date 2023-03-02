0ce0266f01971405957e5aaa56fd101390732223
Ndumatt1 <ndubuisimathew8@gmail.com>
2023-03-02 22:26:51 +0100

    upload 18-binary_tree_uncle

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -5,6 +5,7 @@
  * @node: Pointer to the node to find the unlce
  * Return: Pointer to the uncle node or NULL if node is NULL
  */
+binary_tree_t *binary_tree_siblings(binary_tree_t *node);
 
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
@@ -15,6 +16,30 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 		return (NULL);
 	}
 	parent = node->parent;
-	return (binary_tree_sibling(parent));
+	return (binary_tree_siblings(parent));
 
+}
+
+/**
+ * binary_tree_siblings - finds the sibling of a node
+ * @node: pointer to the node to find the sibling
+ * Return: pointer to the sibling node
+ * NULL if node is NULL
+ * NULL if the parent is NULL
+ * NULL if the node has no siblings
+ */
+binary_tree_t *binary_tree_siblings(binary_tree_t *node)
+{
+	binary_tree_t *parent;
+
+	if (!node || !node->parent)
+	{
+		return (NULL);
+	}
+	parent = node->parent;
+	if (parent->left == node)
+		return (parent->right);
+	if (parent->right == node)
+		return (parent->left);
+	return (NULL);
 }
