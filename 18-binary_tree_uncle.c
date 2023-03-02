40d69d8e8f00b6d6882f650b2057480b94678712
Ndumatt1 <ndubuisimathew8@gmail.com>
2023-03-02 21:45:02 +0100

    upload 18-binary_tree_unce

---
+++ 18-binary_tree_uncle.c
@@ -0,0 +1,23 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_uncle - Finds the uncle of a node
+ * @node: Pointer to the node to find the unlce
+ * Return: Pointer to the uncle node or NULL if node is NULL
+ */
+
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+	binary_tree_t *parent, *grandparent;
+	if (!node)
+	{
+		return (NULL);
+	}
+	parent = node->parent;
+	grandparent = parent->parent;
+
+	if (grandparent == NULL)
+		return (NULL);
+	return (binary_tree_sibling(parent));
+
+}
